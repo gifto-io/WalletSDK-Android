3807cda701ccbef0e5e7054e10ca19eee4490734
Thong Nguyen <nguyenduythong211@gmail.com>
2017-11-07 16:16:39 +0700

    + Clean project before re-upload

--- Android/source/GiftoWalletLibrary/src/main/java/io/gifto/wallet/networking/models/response/NoResponse.java
+++
@@ -1,8 +0,0 @@
-package io.gifto.wallet.networking.models.response;
-
-/**
- * Created by thongnguyen on 10/17/17.
- */
-
-public class NoResponse {
-}
--- Android/source/GiftoWalletLibrary/src/main/java/io/gifto/wallet/utils/common/ICallback.java
+++
@@ -1,9 +0,0 @@
-package io.gifto.wallet.utils.common;
-
-/**
- * Created by ThangPM on 8/5/15.
- */
-public interface ICallback
-{
-    public Object doIt(Object... params);
-}
